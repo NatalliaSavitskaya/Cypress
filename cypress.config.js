e2a78fc4a1ff6c6e07f2c49619fccd8515442aa3
n.savitskaya <for__natasha@mail.ru>
2025-05-07 17:31:43 +0300

    cypress.config.js was configured for running end-to-end tests, base URL was set to https://www.saucedemo.com.

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://www.saucedemo.com',
+    specPattern: 'cypress/e2e/**/*.cy.js', // test files pattern
+    supportFile: 'cypress/support/e2e.js', // optional support file
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
