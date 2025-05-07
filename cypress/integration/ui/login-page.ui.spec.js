d0081b43a1c12837236ca07d9de550fce6019e2a
n.savitskaya <for__natasha@mail.ru>
2025-05-07 20:49:01 +0300

    test cases for testing Login page were added

---
+++ cypress/integration/ui/login-page.ui.spec.js
@@ -0,0 +1,85 @@
+describe('LoginPage: Given Login page opened',{}, () => {
+
+    context.skip(
+        'When user explore the Login page',
+        () => {
+            it('Then user should see the title', () => {});
+            it('Then user should see the URL of Login page', () => {});
+            it('Then user should see the logo', () => {});
+            it('Then user should see the Username field with placeholder', () => {});
+            it('Then user should see the Password field with placeholder', () => {});
+            it('Then user should see the Login button', () => {});
+            it('Then user should see the list of Accepted usernames', () => {});
+            it('Then user should see the Password for all users', () => {});
+        }
+    );
+
+    context.skip(
+        'When user enters valid credentials and clicks Login button',
+        () => {
+            it('Then user should be navigated to the Inventory page', () => {});
+        }
+    );
+
+    context.skip(
+        'When user enters invalid credentials and clicks Login button',
+        () => {
+            it('Then Username and Password fields should be underlined with a red line', () => {});
+            it('Then the error message should be displayed under the Login button', () => {});
+            it('Then the cross icon should be displayed to the right of Username field', () => {});
+            it('Then the cross icon should be displayed to the right of Password field', () => {});
+            it('Then the cross icon should be displayed to the right of error message', () => {});
+    }
+);
+
+    context.skip(
+        'When user leaves Username field empty and enters valid Password and clicks Login button',
+        () => {
+            it('Then Username and Password fields should be underlined with a red line', () => {});
+            it('Then the error message should be displayed under the Login button', () => {});
+            it('Then the cross icon should be displayed to the right of Username field', () => {});
+            it('Then the cross icon should be displayed to the right of Password field', () => {});
+            it('Then the cross icon should be displayed to the right of error message', () => {});
+        }
+    );
+
+    context.skip(
+        'When user enters valid Username and leaves Password field empty and clicks Login button',
+        () => {
+            it('Then Username and Password fields should be underlined with a red line', () => {});
+            it('Then the error message should be displayed under the Login button', () => {});
+            it('Then the cross icon should be displayed to the right of Username field', () => {});
+            it('Then the cross icon should be displayed to the right of Password field', () => {});
+            it('Then the cross icon should be displayed to the right of error message', () => {});
+        }
+    );
+
+    context.skip(
+        'When user leaves Username and Password fields empty and clicks Login button',
+        () => {
+            it('Then Username and Password fields should be underlined with a red line', () => {});
+            it('Then the error message should be displayed under the Login button', () => {});
+            it('Then the cross icon should be displayed to the right of Username field', () => {});
+            it('Then the cross icon should be displayed to the right of Password field', () => {});
+            it('Then the cross icon should be displayed to the right of error message', () => {});
+        }
+    );
+
+    context.skip(
+        'When user enters valid Username in uppercase and enters valid Password and clicks Login button',
+        () => {
+            it('Then Username and Password fields should be underlined with a red line', () => {});
+            it('Then the error message should be displayed under the Login button', () => {});
+            it('Then the cross icon should be displayed to the right of Username field', () => {});
+            it('Then the cross icon should be displayed to the right of Password field', () => {});
+            it('Then the cross icon should be displayed to the right of error message', () => {});
+        }
+    );
+
+    context.skip(
+        'When logged in user clicks Logout button',
+        () => {
+            it('Then user should be navigated to the Login page', () => {});
+        }
+    );
+});
